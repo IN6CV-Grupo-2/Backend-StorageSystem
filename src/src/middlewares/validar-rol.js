4b03256a1579f81f1526d5ca324363ccfcf997e4
TGManuel <168132719+TGManuel@users.noreply.github.com>
2025-05-02 20:23:42 -0600

    Delete src/src directory

--- src/src/middlewares/validar-rol.js
+++
@@ -1,9 +0,0 @@
-export const validarRol = (...rolesPermitidos) => {
-    return (req, res, next) => {
-      if (!rolesPermitidos.includes(req.user.role)) {
-        return res.status(403).json({ msg: 'Acceso denegado: rol no autorizado' });
-      }
-      next();
-    };
-  };
-  
